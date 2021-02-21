b62afa3a31df9a94bd03b85ae17586c39d81bf85
Lisbeth Purrucker <lisbeth@cuba-buddy.de>
2021-02-21 22:10:43 +0100

    changed complete style

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root to: "cocktails#index"
   resources :cocktails do
     resources :doses, only: [:new, :create, :edit, :update]
-    resources :reviews, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
 end
